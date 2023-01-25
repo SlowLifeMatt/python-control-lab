1ac5977e05e1b152f8c26884c6074259aa0e9d19
Matthew <subinmatt@gmail.com>
2023-01-24 22:25:32 -0800

    completed question 3

--- control-flow-python-lab.py
+++ control-flow-python-lab.py
@@ -49,11 +49,11 @@ if human_age <= 2:
     dog_age = human_age * 10
 #      - Any remaining years count as 7 years each
 else:
-    dog_age = human_age * 7
+    dog_age = (2 * 10) + (human_age -2) * 7
 # 3. Prints the answer in the following format:
 #      The dog's age in dog years is xx
 # Hint:  Use the int() function to convert the string returned from input() into an integer
-print("The dog's age in dog years is", dog_age)
+print(f"The dog's age in dog years is{dog_age}")
 
 
 # exercise-04 What kind of Triangle?
