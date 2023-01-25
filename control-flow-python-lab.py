531eba480f52a383d4623339cb3bc397d3509184
Matthew <subinmatt@gmail.com>
2023-01-24 22:08:48 -0800

    second question completed

--- control-flow-python-lab.py
+++ control-flow-python-lab.py
@@ -26,8 +26,12 @@ else:
 # Write the code that:
 # 1. Prompts the user to enter a phrase:
 #      Please enter a word or phrase:
+phrase = ""
+while phrase != 'quit':
+    phrase = input("Enter a word or phrase: ")
 # 2. Print the following message:
 #      - What you entered is xx characters long
+    print("What you entered is", len(phrase), "characters long")
 # 3. Return to step 1, unless the word 'quit' was entered.
 
 
